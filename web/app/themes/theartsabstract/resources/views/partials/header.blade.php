355d35b7369fdf90eac4a45399aa864f05ab981e
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2019-09-02 20:56:55 -0400

    Fix cursor on menu

--- web/app/themes/theartsabstract/resources/views/partials/header.blade.php
+++ web/app/themes/theartsabstract/resources/views/partials/header.blade.php
@@ -8,7 +8,7 @@
 
         <input type="checkbox" class="hidden" id="mobile-menu-toggle">
 
-        <label for="mobile-menu-toggle" class="text-white">
+        <label for="mobile-menu-toggle" class="text-white cursor-pointer">
             <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
         </label>
 
