4b637cf2e96c6df20ff8efd12a5af0ef83ddd4d6
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-10-24 10:39:28 -0400

    Wip

--- web/app/themes/theartsabstract/index.php
+++ web/app/themes/theartsabstract/index.php
@@ -7,7 +7,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&family=Vollkorn:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-        <script defer src="https://cdn.usefathom.com/script.js" site="CBGVYXOH" included-domains="theartsabstract.ca"></script>
+        <script defer src="https://cdn.usefathom.com/script.js" data-site="CBGVYXOH" data-included-domains="theartsabstract.ca"></script>
     </head>
     <body <?php body_class(['flex', 'flex-col', 'min-h-screen']); ?>>
         <?php wp_body_open(); ?>
